0aebd969a9780367013487ba9e9d7564493bccd8
Pol Riubrogent <polriubro7@gmail.com>
2026-02-18 16:25:40 +0100

    initial commit

---
+++ install_c5.sh
@@ -0,0 +1,8 @@
+conda create -n c5 python=3.10 -y
+conda activate c5
+pip install torch torchvision
+pip install transformers
+pip install ultralytics
+pip install albumentations
+pip install pycocotools
+echo "Done"
